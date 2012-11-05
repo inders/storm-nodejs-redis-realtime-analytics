8bdeea7c6984adfe6f2338adc4573198c0f59a92
Jaydeep Vishwakarma <jaydeep.vishwakarma@450291asf5w.mkhoj.com>
2012-11-05 20:03:07 +0530

    added sentiment bolt channel

---
+++ src/jvm/storm/starter/bolt/ImobiSentimentBolt.java
@@ -0,0 +1,52 @@
+package storm.starter.bolt;
+
+
+import java.util.List;
+
+
+import storm.starter.common.Entry;
+
+import com.google.gson.Gson;
+
+import storm.starter.bolt.RedisBolt.OnDynamicConfigurationListener;
+import twitter4j.Status;
+
+/**
+ * 
+ * This bolt will get an url coming from the android market and will try to get the html and parse the information, publishing into a redis
+ * channel the information recollected.
+ * 
+ * @author arcturus@ardeenelinfierno.com
+ * 
+ */
+public class ImobiSentimentBolt extends RedisBolt implements OnDynamicConfigurationListener {
+
+  @Override
+  protected void setupNonSerializableAttributes() {
+    // TODO Auto-generated method stub
+    super.setupNonSerializableAttributes();
+    setupDynamicConfiguration(this);
+  }
+
+  @Override
+  public List<Object> filter(Status status) {
+    Gson gson = new Gson();
+    String jsonString = (String) currentTuple.getValue(0);
+    if (jsonString == null) {
+      return null;
+    }
+
+    try {
+      Entry entry = gson.fromJson(jsonString, Entry.class);
+      publish(jsonString, entry.getChannel());
+    } catch (Exception e) {
+      return null;
+    }
+    return null;
+  }
+
+  @Override
+  public void onConfigurationChange(String conf) {
+  }
+
+}
