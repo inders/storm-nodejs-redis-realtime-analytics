202e5b89d15e6f069a1f3f0199de0b537fd71d77
netra-shetty <netra.shetty@inmobi.com>
2012-11-05 21:21:23 +0530

    classifier script added

---
+++ classifier/classifier.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mallet="/home/netra/mallet/mallet-2.0.7/bin/mallet"
+mkdir buzztest
+echo $1 > buzztest/file.txt
+$mallet classify-dir --input buzztest --classifier maxentbigrams.classifier --output buzztest/o.txt
+awk -F, 'BEGIN { FS = " " } ; { if($3 > $5) {print $2 , $3;} else {print $4, $5;}}' buzztest/o.txt ; 
+rm -rf buzztest
