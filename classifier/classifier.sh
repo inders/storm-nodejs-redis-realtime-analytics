3e2552e4ca425cc07d61bca8f7138552bc77a889
netra-shetty <netra.shetty@inmobi.com>
2012-11-06 16:18:34 +0530

    added twitter trained classifier

--- classifier/classifier.sh
+++ classifier/classifier.sh
@@ -2,6 +2,6 @@
 mallet="/home/netra/mallet/mallet-2.0.7/bin/mallet"
 mkdir buzztest
 echo $1 > buzztest/file.txt
-$mallet classify-dir --input buzztest --classifier maxentbigrams.classifier --output buzztest/o.txt
+$mallet classify-dir --input buzztest --classifier tweet_maxent.classifier --output buzztest/o.txt
 awk -F, 'BEGIN { FS = " " } ; { if($3 > $5) {print $2 , $3;} else {print $4, $5;}}' buzztest/o.txt ; 
 rm -rf buzztest
