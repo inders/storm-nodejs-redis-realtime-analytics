5b330947280fb435d8e9317a79e447287d4d2810
jaideep.dhok <jaideep.dhok@inmobi.com>
2012-12-03 18:36:18 +0530

    save ticks in ES

--- node/buzz.js
+++ node/buzz.js
@@ -1,6 +1,7 @@
 /** Imports **/
 var express = require('express')
- ,   app = express()  
+ ,   app = express() 
+ , http = require('http')
  , server = require('http').createServer(app)
  , io = require('socket.io').listen(server)
  , redis = require('redis')
@@ -23,18 +24,11 @@ var NEUT = "neutral";
 var labels = [POS, NEG, NEUT];
 var channels = ["GoogleAlerts"];
 
+var ES_URI = 'http://localhost:9200/ticks/'
+
 var makeTick = function(inmsgStr) {
     var inmsg = JSON.parse(inmsgStr);
-    /*
-    var channelStr = inmsg.channel;
-    if (channelStr.match(/^positive\_/)) {
-        inmsg.sentiment = POS;
-    } else if (channelStr.match(/^negative\_/)) {
-        inmsg.sentiment = NEG;
-    } else {
-        inmsg.sentiment = NEUT;
-    }
-    */
+    console.log('>>>> sentiment ' + inmsg.sentiment);
 
     var outmsg =  {
         channel: inmsg.channel,
@@ -70,10 +64,52 @@ for (var i = 0; i < channels.length; i++) {
     redisClient.subscribe(channels[i]);
 }
 
+/*
+var cl2 = redis.createClient();
+var negid = 1;
+redisClient.on('subscribe', function(channel, count) {
+    console.log('SUBSCRIBED ' + channel);
+    if ('GoogleAlerts' === channel) { 
+        console.log('>>> Setting timer')
+        setInterval(function () {
+            negid += 1;
+            cl2.publish('GoogleAlerts', JSON.stringify({
+                channel: 'GoogleAlerts',
+                sentiment: NEG ,
+                id: '' + negid,
+                title: 'Glasdoor Employee Review',
+                link: 'http://www.glassdoor.com/Reviews/Employee-Review-InMobi-RVW2045626.htm'
+            }));
+            console.log('###### Sent negative tick');
+        }, 5000);
+    }
+});
+*/
+
+var saveInES = function(message) {
+    var options = {
+      hostname: 'localhost',
+      port: 9200,
+      path: '/ticks/' + message.channel + '/' + message.id,
+      method: 'PUT'
+    };
+
+    var req = http.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+    });
+
+    req.write(JSON.stringify(message) + '\n');
+    req.end();
+    console.log('$#$#$#$#$#$#$#$#$ SAVED IN ES');
+};
+    
 redisClient.on('message', function (channel, message) {
     // Broadcast message to all clients
-    io.sockets.emit('tick', makeTick(message));
-    console.log('New tick from STORM: ' + channel);
+    var tick = makeTick(message);
+    saveInES(tick);
+    io.sockets.emit('tick', tick);
 });
+
 
 
